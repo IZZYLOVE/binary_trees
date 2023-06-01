b831c14f5bb3002aa3403c01d98f8cb70123afd8
solokool66 <solokool66@gmail.com>
2023-06-01 09:34:51 -0700

    14-binary_tree_balance.c updated task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,11 +14,11 @@ return (0);
 }
 
 /**
- * binary_tree_height - This measures the height of a binary tree.
- * @tree: Points to the root node of the tree to measure the height.
- *
- * Return: If tree is NULL, your function must return 0, else return height.
- */
+* binary_tree_height - This measures the height of a binary tree.
+* @tree: Points to the root node of the tree to measure the height.
+*
+* Return: If tree is NULL, your function must return 0, else return height.
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
