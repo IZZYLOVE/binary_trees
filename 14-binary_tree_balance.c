0b6f9832c500abc4649c0884b92005541a719e51
solokool66 <solokool66@gmail.com>
2023-06-01 09:29:12 -0700

    10-binary_tree_depth.c updated task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,17 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - This measures the balance factor of a binary tree.
- * @tree: Points to the root node of the tree to measure the balance factor.
- *
- * Return: If tree is NULL, return 0, else return balance factor.
- */
+* binary_tree_balance - This measures the balance factor of a binary tree.
+* @tree: Points to the root node of the tree to measure the balance factor.
+*
+* Return: If tree is NULL, return 0, else return balance factor.
+*/
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-
-	return (0);
+if (tree)
+return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+return (0);
 }
 
 /**
